233e6cc2cf7bb76f1881f3b81d1826127c4f74d3
rapha <rapha.fayebiz@gmail.com>
2023-08-26 09:37:04 +0000

    bg fixing

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -16,12 +16,12 @@ module.exports = {
 				'custom-double': 'rgba(50, 50, 93, 0.25) 0px 50px 100px -20px, rgba(0, 0, 0, 0.3) 0px 30px 60px -30px',
 			},
 			backgroundImage: theme => ({
-				'hero-desk': "url('images/homepage/hero-bg-desktop@2x.jpg')",
-				'hero-mob': "url('images/homepage/hero-bg-mobile.jpg')",
-				'bg-ready': "url('images/homepage/ready-bg-desktop@2x.jpg')",
-				'bg-ready-mob': "url('images/homepage/ready-bg-mobile.jpg')",
-				'bg-book': "url('images/booking/hero-bg-desktop@2x.jpg')",	
-				'bg-book-mob': "url('images/booking/hero-bg-mobile.jpg')",	
+				'hero-desk': "url('/images/homepage/hero-bg-desktop@2x.jpg')",
+				'hero-mob': "url('/images/homepage/hero-bg-mobile.jpg')",
+				'bg-ready': "url('/images/homepage/ready-bg-desktop@2x.jpg')",
+				'bg-ready-mob': "url('/images/homepage/ready-bg-mobile.jpg')",
+				'bg-book': "url('/images/booking/hero-bg-desktop@2x.jpg')",	
+				'bg-book-mob': "url('/images/booking/hero-bg-mobile.jpg')",	
 			})
 		},
 	},
